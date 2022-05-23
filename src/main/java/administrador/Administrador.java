23a556f82c3808fed9617dadc9240f9f86a143a2
julian-salinas <juliansalinasm@gmail.com>
2022-05-23 01:19:27 -0300

    reflejo modificaciones hechas sobre contraseñas en el diagrama

--- src/main/java/administrador/Administrador.java
+++ src/main/java/administrador/Administrador.java
@@ -4,11 +4,11 @@ import passwords.Contrasenia;
 
 public class Administrador {
   String user;
-  Contrasenia password;
+  Contrasenia contrasenia;
 
-  public Administrador(String user, Contrasenia password) {
+  public Administrador(String user, Contrasenia contrasenia) {
     this.user = user;
-    this.password = password;
+    this.contrasenia = contrasenia;
   }
 
 }
