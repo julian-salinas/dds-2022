1fc3f0f5637c2faeda9a98527ad16d4c069a5e9c
LNArrUniv <larrieta@frba.utn.edu.ar>
2022-06-08 18:47:43 -0300

    Cambio valores de enums TC

--- src/main/java/domain/organizaciones/consumos/tipos/GasNatural.java
+++ src/main/java/domain/organizaciones/consumos/tipos/GasNatural.java
@@ -1,13 +1,13 @@
 package domain.organizaciones.consumos.tipos;
 
-import static domain.organizaciones.consumos.Actividad.COMBUSTIONFIJA;
+import static domain.organizaciones.consumos.Actividad.COMBUSTION_FIJA;
 import static domain.organizaciones.consumos.Alcance.DIRECTAS;
 import static domain.organizaciones.consumos.Unidad.M3;
 
 public class GasNatural extends TipoDeConsumo {
   public GasNatural() {
     unidad = M3;
-    actividad = COMBUSTIONFIJA;
+    actividad = COMBUSTION_FIJA;
     alcance = DIRECTAS;
   }
 }
--- src/main/java/domain/organizaciones/consumos/tipos/Nafta.java
+++ src/main/java/domain/organizaciones/consumos/tipos/Nafta.java
@@ -1,13 +1,13 @@
 package domain.organizaciones.consumos.tipos;
 
-import static domain.organizaciones.consumos.Actividad.COMBUSTIONFIJA;
+import static domain.organizaciones.consumos.Actividad.COMBUSTION_FIJA;
 import static domain.organizaciones.consumos.Alcance.DIRECTAS;
 import static domain.organizaciones.consumos.Unidad.LT;
 
 public class Nafta extends TipoDeConsumo {
   public Nafta() {
     unidad = LT;
-    actividad = COMBUSTIONFIJA;
+    actividad = COMBUSTION_FIJA;
     alcance = DIRECTAS;
   }
 }
