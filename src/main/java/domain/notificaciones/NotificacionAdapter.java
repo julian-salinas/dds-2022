7f63e9985ca1799ae58342975c15e3f111b8a650
julian-salinas <juliansalinasm@gmail.com>
2022-07-14 08:33:27 -0300

    agrego atributo contactos en organizacion

--- src/main/java/domain/notificaciones/NotificacionAdapter.java
+++ src/main/java/domain/notificaciones/NotificacionAdapter.java
@@ -8,7 +8,7 @@ public interface NotificacionAdapter {
    * @name: enviar
    * @param contacto: Contacto al cual se enviará la notificación mediante todas sus suscripciones
    * @param mensaje: Mensaje a enviar
-   * @return status code
+   * @return status code. -1 indica error de ejecución
    */
   int enviar(Contacto contacto, String mensaje);
 
