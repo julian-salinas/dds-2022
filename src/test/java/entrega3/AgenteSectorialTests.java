2146396312643f23d5f8bf6e2922138230048432
GonzaloMartinezSa <gmartnezsarmiento@frba.utn.edu.ar>
2022-08-04 18:14:02 -0300

    Deleteo algunos tests y empiezo con tests de Agentes Sec.

---
+++ src/test/java/entrega3/AgenteSectorialTests.java
@@ -0,0 +1,16 @@
+package entrega3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class AgenteSectorialTests {
+
+  @Test
+  public void soyUnTest() {
+    // TODO
+  }
+
+}
