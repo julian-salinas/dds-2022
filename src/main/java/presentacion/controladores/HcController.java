33eefc7008800dc61ee1bddfa7ca13d8edeaf049
mati Bellini <matiasalejo2010@gmail.com>
2022-10-22 23:40:16 -0300

    muestra valor HC anual y mensual

---
+++ src/main/java/presentacion/controladores/HcController.java
@@ -0,0 +1,29 @@
+package presentacion.controladores;
+
+import domain.organizaciones.Organizacion;
+import domain.repositorios.RepositorioUsuarios;
+import presentacion.Usuario;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HcController {
+
+  public ModelAndView index(Request request, Response response) {
+
+    String username = request.cookie("username");
+    Usuario user = RepositorioUsuarios.getInstance().findByUsername(username);
+
+    Organizacion organizacion = user.getOrg();
+
+    Map<String, Object> model = new HashMap<>();
+      model.put("mensual", organizacion.hcMensual().enKgCO2());
+      model.put("anual", organizacion.hcAnual().enKgCO2());
+
+    return new ModelAndView(model, "hc.hbs");
+  }
+
+}
